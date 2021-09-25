4e1ea5d98211a97170f6d5285f7aed3d94e4a4b1
Simon Castillo <simoncastillo@gmail.com>
2021-09-25 21:22:29 +0100

    Add my initial setup

---
+++ config.el
@@ -0,0 +1,175 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Simon Castillo"
+      user-mail-address "redacted")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+(setq doom-font (font-spec :family "mononoki" :size 12))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+(setq lsp-clients-clangd-args '("-j=3"
+                                "--background-index"
+                                "--clang-tidy"
+                                "--completion-style=detailed"
+                                "--header-insertion=never"
+                                "--header-insertion-decorators=0"))
+(after! lsp-clangd (set-lsp-priority! 'clangd 2))
+
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier 'super)
+(setq ns-function-modifier 'alt)
+
+(global-subword-mode)
+
+(defun goto-match-paren (arg)
+  "Go to the matching  if on (){}[], similar to vi style of % "
+  (interactive "p")
+  ;; first, check for "outside of bracket" positions expected by
+  ;; forward-sexp, etc
+  (cond ((looking-at "[\[\(\{]") (forward-sexp))
+        ((looking-back "[\]\)\}]" 1) (backward-sexp))
+        ;; now, try to succeed from inside of a bracket
+        ((looking-at "[\]\)\}]") (forward-char) (backward-sexp))
+        ((looking-back "[\[\(\{]" 1) (backward-char) (forward-sexp))
+        (t nil)))
+
+(global-set-key (kbd "M-p") 'goto-match-paren)
+
+(use-package! highlight-parentheses
+  :hook (prog-mode . highlight-parentheses-mode))
+
+(use-package! rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package! highlight-symbol
+  :init
+  (defhydra hydra-highlight-symbol (global-map "C-c #")
+    "highlight-symbol"
+    ("#" highlight-symbol)
+    ("n" highlight-symbol-next)
+    ("p" highlight-symbol-prev)
+    ("%" highlight-symbol-query-replace)
+    ("a" highlight-symbol-remove-all))
+
+  (defhydra hydra-highlight-symbol (global-map "C-c £")
+    "highlight-symbol"
+    ("£" highlight-symbol)
+    ("n" highlight-symbol-next)
+    ("p" highlight-symbol-prev)
+    ("%" highlight-symbol-query-replace)
+    ("a" highlight-symbol-remove-all))
+  )
+
+(use-package! multiple-cursors
+  :init
+  (global-set-key (kbd "C-<return>") 'set-rectangular-region-anchor)
+  (global-set-key (kbd "C->") 'mc/mark-next-like-this)
+  (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+  (global-set-key (kbd "C-c M a") 'mc/mark-all-like-this))
+
+(use-package! windmove
+  :init
+  (windmove-default-keybindings))
+
+(use-package! zop-to-char
+  :init
+  (global-set-key (kbd "M-z") 'zop-up-to-char)
+  (global-set-key (kbd "M-Z") 'zop-to-char))
+
+(use-package! crux
+  :init
+  (global-set-key (kbd "C-+") 'text-scale-increase)
+  (global-set-key (kbd "C--") 'text-scale-decrease)
+  (global-set-key (kbd "C-x m") 'eshell)
+  (global-set-key (kbd "C-c d") 'crux-duplicate-current-line-or-region)
+  (global-set-key [(shift return)] 'crux-smart-open-line)
+  (global-set-key (kbd "M-o") 'crux-smart-open-line)
+  (global-set-key [(control shift return)] 'crux-smart-open-line-above)
+  (global-set-key [(control shift up)]  'move-text-up)
+  (global-set-key [(control shift down)]  'move-text-down))
+
+(after! consult (global-set-key (kbd "C-c ! !") 'consult-flycheck))
+
+(use-package! uniquify
+  :init
+  (setq uniquify-buffer-name-style 'forward)
+  (setq uniquify-separator "/")
+  (setq uniquify-after-kill-buffer-p t)    ; rename after killing uniquified
+  (setq uniquify-ignore-buffers-re "^\\*") ; don't muck with special buffers
+  )
+
+(use-package! easy-kill
+  :init
+  (global-set-key [remap kill-ring-save] 'easy-kill)
+  (global-set-key [remap mark-sexp] 'easy-mark))
+
+(use-package! operate-on-number
+  :init
+  (require 'smartrep)
+  (smartrep-define-key global-map "C-c ."
+  '(("+" . apply-operation-to-number-at-point)
+    ("-" . apply-operation-to-number-at-point)
+    ("*" . apply-operation-to-number-at-point)
+    ("/" . apply-operation-to-number-at-point)
+    ("\\" . apply-operation-to-number-at-point)
+    ("^" . apply-operation-to-number-at-point)
+    ("<" . apply-operation-to-number-at-point)
+    (">" . apply-operation-to-number-at-point)
+    ("#" . apply-operation-to-number-at-point)
+    ("%" . apply-operation-to-number-at-point)
+    ("'" . operate-on-number-at-point)))
+)
+
+(use-package! anzu
+  :init
+  (global-set-key (kbd "M-%") 'anzu-query-replace)
+  (global-set-key (kbd "C-M-%") 'anzu-query-replace-regexp))
+
+(setq-default cursor-type 'bar)
